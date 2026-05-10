e00fa4da15e5348c73e1a7b0fb9cec57de528ce2
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-05-10 21:51:59 +0530

    refactor: fluent API for global UI config
    - migrate `UIManagerConfigurations to `GlobalUIConfiguration`
    - also allow for manual theme setup

--- src/main/java/io/github/eggy03/nautilus/linux/Start.java
+++ src/main/java/io/github/eggy03/nautilus/linux/Start.java
@@ -4,13 +4,11 @@
  */
 package io.github.eggy03.nautilus.linux;
 
-import com.formdev.flatlaf.FlatLaf;
 import io.github.eggy03.nautilus.linux.ui.primary.MainUI;
 import io.github.eggy03.nautilus.linux.ui.secondary.ExceptionUI;
 import io.github.eggy03.nautilus.linux.ui.themes.StandardDarkTheme;
+import io.github.eggy03.nautilus.linux.utility.GlobalUIConfiguration;
 import io.github.eggy03.nautilus.linux.utility.OSDetection;
-import io.github.eggy03.nautilus.linux.utility.UIManagerConfigurations;
-import io.github.eggy03.theme.manager.ThemeManager;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.EventQueue;
@@ -23,17 +21,13 @@ public class Start {
      */
     public static void main(String[] args) {
 
-        ThemeManager themeManager = new ThemeManager(Start.class);
-
         log.info("Detected OS: {}", OSDetection.getCurrentOS());
-        FlatLaf.registerCustomDefaultsSource("themes"); // for maven build, this points towards src/main/resources/themes
 
         EventQueue.invokeLater(() -> {
-            themeManager.loadAndApplyRegisteredThemeOrFallback(StandardDarkTheme.class.getName());
-            themeManager.loadAndApplyColorFilter();
-
-            UIManagerConfigurations.enableRoundComponents();
-            UIManagerConfigurations.enableTabSeparators(true);
+            new GlobalUIConfiguration()
+                    .applyCustomLookAndFeel(StandardDarkTheme.class.getName())
+                    .enableRoundComponents()
+                    .enableTabSeparators(true);
 
             launchUIBasedOnOS();
         });
