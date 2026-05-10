addd30a8d962e8b9e588a9cee7bee498be6f6688
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-05-10 20:32:22 +0530

    refactor: rename base package
    - Rename package `io.github.eggy03.ui` to `io.github.eggy03.nautilus.linux`

---
+++ src/main/java/io/github/eggy03/nautilus/linux/Start.java
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2026 Egg-03
+ */
+package io.github.eggy03.nautilus.linux;
+
+import com.formdev.flatlaf.FlatLaf;
+import io.github.eggy03.theme.manager.ThemeManager;
+import io.github.eggy03.nautilus.linux.common.constant.OSConstants;
+import io.github.eggy03.nautilus.linux.common.themes.StandardDarkTheme;
+import io.github.eggy03.nautilus.linux.common.ui.ExceptionUI;
+import io.github.eggy03.nautilus.linux.common.utilities.UIManagerConfigurations;
+import io.github.eggy03.nautilus.linux.linux.LinuxUI;
+import lombok.extern.slf4j.Slf4j;
+
+import java.awt.EventQueue;
+import java.util.Objects;
+
+@Slf4j
+public class Start {
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+
+        ThemeManager themeManager = new ThemeManager(Start.class);
+
+        log.info("Detected OS: {}", OSConstants.getCurrentOS());
+        FlatLaf.registerCustomDefaultsSource("themes"); // for maven build, this points towards src/main/resources/themes
+
+        EventQueue.invokeLater(()-> {
+            themeManager.loadAndApplyRegisteredThemeOrFallback(StandardDarkTheme.class.getName());
+            themeManager.loadAndApplyColorFilter();
+
+            UIManagerConfigurations.enableRoundComponents();
+            UIManagerConfigurations.enableTabSeparators(true);
+            
+            launchUIBasedOnOS();
+        });
+    }
+
+    private static void launchUIBasedOnOS() {
+        if (Objects.requireNonNull(OSConstants.detectOs()) == OSConstants.LINUX) {
+            new LinuxUI().setVisible(true);
+        } else {
+            new ExceptionUI("Unsupported OS", OSConstants.getCurrentOS() + " is not supported");
+        }
+    }
+}
