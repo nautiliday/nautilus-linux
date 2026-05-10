0d0008f43ee467e3019b6c1d832132677bfb75d6
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-05-10 21:52:26 +0530

    chore: update version and release page

--- src/main/java/io/github/eggy03/nautilus/linux/constant/Version.java
+++ src/main/java/io/github/eggy03/nautilus/linux/constant/Version.java
@@ -9,10 +9,10 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Version {
 
-    public static final String SEMVER = "1.0.0";
-    public static final String SUFFIX = "-SNAPSHOT";
-    public static final String BUILD_DATE = "20260428";
+    public static final String SEMVER = "0.1.0";
+    public static final String SUFFIX = "";
+    public static final String BUILD_DATE = "";
     public static final String DEVELOPER = "Egg-03";
     public static final String LICENSE = "GNU General Public License v3.0";
-    public static final String GITHUB_PAGE = "https://github.com/eggy03/Nautilus";
+    public static final String GITHUB_PAGE = "https://github.com/nautiliday/nautilus-linux";
 }
